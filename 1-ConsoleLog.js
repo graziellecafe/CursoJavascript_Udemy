2bd73221e83f0d0bd45783f2af3265ef47aa7cee
Grazielle Amanda do Carmo Café <grazielle.cafe@dtidigital.com.br>
2021-08-29 11:45:57 -0300

    Impressão utilizando o console.log

---
+++ 1-ConsoleLog.js
@@ -0,0 +1,10 @@
+// 1. console.log
+
+// Formas de Impressão de string: 
+console.log('Grazielle Amanda');
+console.log("Grazielle Amanda"); 
+console.log(`Grazielle Amanda`); // mais avançado
+
+// Imprimindo números
+console.log(2); //número inteiro -- number
+console.log(0.2); // número real ou número de ponto flutuante no JavaScript, utiliza-se . -- number
