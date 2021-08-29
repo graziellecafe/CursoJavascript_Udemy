35d85882bf7fb75f2a606f22f02da58911e6eb71
Grazielle Amanda do Carmo Café <grazielle.cafe@dtidigital.com.br>
2021-08-29 11:54:14 -0300

    Realizando o exercício da aula

--- 1-ConsoleLog.js
+++ 1-ConsoleLog.js
@@ -8,3 +8,10 @@ console.log(`Grazielle Amanda`); // mais avançado
 // Imprimindo números
 console.log(2); //número inteiro -- number
 console.log(0.2); // número real ou número de ponto flutuante no JavaScript, utiliza-se . -- number
+
+console.log(`Olá! Sou a "Grazielle" e estou estudando Javascript às 11 da manhã. `);
+console.log('Olá, sou a "Grazielle" e estou aprendendo JavaScript às', 11, 'da manhã');
+
+const nome = 'Grazielle'; 
+const idade = 27; 
+console.log(`Olá, meu nome é ${nome} e tenho ${idade} anos.`); 
